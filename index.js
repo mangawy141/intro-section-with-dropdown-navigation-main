cc6962d9d548db0524b0ba81783119bbb4bca5cb
Ahmed_Agha__ <115808351+mangawy141@users.noreply.github.com>
2024-04-14 16:40:01 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -24,45 +24,43 @@ company.addEventListener("click", function () {
 // SIDEBAR -----------------
 let sidebar = document.querySelector(".sidebar");
 let sidebar_icon = document.querySelector(".sidebar img");
-let mobile_menu = document.querySelector('.mobile-menu ul');
+let mobile_menu = document.querySelector(".mobile-menu ul");
 
 let clicked = true;
 sidebar.addEventListener("click", function () {
   if (clicked) {
     sidebar_icon.setAttribute("src", "images/icon-close-menu.svg");
-    mobile_menu.style.display = "flex";
-    mobile_menu.style.width = "65vw";
-    mobile_menu.classList.add('sidebar-shadow')
+    mobile_menu.classList.add("sidebar-shadow");
+    mobile_menu.classList.toggle("slide");
     clicked = false;
   } else {
     sidebar_icon.setAttribute("src", "images/icon-menu.svg");
-    mobile_menu.style.display = "none";
-    mobile_menu.style.width = "0";
-    mobile_menu.classList.remove('sidebar-shadow')
-    // mobile_menu.style.display = "none";
-
+    mobile_menu.classList.remove("sidebar-shadow");
+    mobile_menu.classList.toggle("slide");
     clicked = true;
   }
 });
 
-let mobile_menu_features = document.querySelector('.mobile-menu .features')
-let mobile_menu_arw1 = document.querySelector('.mobile-menu .features img');
-let mobile_menu_feat_drop = document.querySelector(".mobile-menu .features-drop-down");
+let mobile_menu_features = document.querySelector(".mobile-menu .features");
+let mobile_menu_arw1 = document.querySelector(".mobile-menu .features img");
+let mobile_menu_feat_drop = document.querySelector(
+  ".mobile-menu .features-drop-down"
+);
 
 mobile_menu_features.addEventListener("click", function () {
   mobile_menu_arw1.classList.toggle("clicked-arrows");
   mobile_menu_feat_drop.classList.toggle("showIt");
-  mobile_menu_feat_drop.style.position = "relative"
+  mobile_menu_feat_drop.style.position = "relative";
 });
 
 let mobile_menu_company = document.querySelector(".mobile-menu .company");
-let mobile_company_arrow = document.querySelector(
-  ".mobile-menu .company img"
+let mobile_company_arrow = document.querySelector(".mobile-menu .company img");
+let mobile_company_drop_down = document.querySelector(
+  ".mobile-menu .company-drop-down"
 );
-let mobile_company_drop_down = document.querySelector(".mobile-menu .company-drop-down");
 
 mobile_menu_company.addEventListener("click", function () {
   mobile_company_arrow.classList.toggle("clicked-arrows");
   mobile_company_drop_down.classList.toggle("showIt");
-  mobile_company_drop_down.style.position = "relative"
+  mobile_company_drop_down.style.position = "relative";
 });
